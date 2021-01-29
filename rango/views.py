8bbffd63d96039c76629e5309fe751620c6a260b
xinming Feng <2584847f@student.gla.ac.uk>
2021-01-29 23:24:08 +0800

    second commit

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango say hey there partner!<a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage':'Crunchy,creamy,cookie,candy,cupcake!'}
+    return render(request,'rango/index.html',context=context_dict)
+    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
 
 
 def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by xinming Feng.'}
+    return render(request,'rango/about.html',context=context_dict)
     return HttpResponse("Rango says here is the page.<a href='/rango/'>Index</a>")
+    
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -64,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -118,5 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
